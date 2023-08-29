ecebf7497773caa67198496685cd4e602faff6e0
Bmogul <98663272+Bmogul@users.noreply.github.com>
2023-08-29 01:03:13 -0400

    mobile freidny navbar done

--- src/js/script.js
+++ src/js/script.js
@@ -7,28 +7,36 @@ window.addEventListener('scroll', () => {
     console.log(window.scrollY);
     let currentScrollPos = window.scrollY;
 
-    if (currentScrollPos > prevScrollpos) {
-        // Scrolling down
-        scrolledDistance = 0;
-    } else {
-        // Scrolling up
-        scrolledDistance += (prevScrollpos - currentScrollPos);
-    }
-
-    if(currentScrollPos <= 80){
+    // Check if the screen width is less than or equal to 768px (typical mobile width)
+    if (window.innerWidth <= 768) {
+        // Do not fade out the navbar on mobile
         navbar.style.animation = 'fadeIn 0.5s';
         navbar.classList.remove('transparent');
-    }else{
-        if (scrolledDistance > 100) {
+    } else {
+        if (currentScrollPos > prevScrollpos) {
+            // Scrolling down
+            scrolledDistance = 0;
+        } else {
+            // Scrolling up
+            scrolledDistance += (prevScrollpos - currentScrollPos);
+        }
+
+        if(currentScrollPos <= 80){
             navbar.style.animation = 'fadeIn 0.5s';
             navbar.classList.remove('transparent');
-        } else {
-            navbar.style.animation = 'fadeOut 0.5s';
-            navbar.classList.add('transparent');
+        }else{
+            if (scrolledDistance > 100) {
+                navbar.style.animation = 'fadeIn 0.5s';
+                navbar.classList.remove('transparent');
+            } else {
+                navbar.style.animation = 'fadeOut 0.5s';
+                navbar.classList.add('transparent');
+            }
         }
     }
 
-
-
     prevScrollpos = currentScrollPos;
 });
+
+
+
