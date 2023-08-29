bcdcf54128f19aedb42d4ebc266efd946a2b6b00
Bmogul <98663272+Bmogul@users.noreply.github.com>
2023-08-28 20:46:07 -0400

    added webpack functionality

---
+++ src/js/script.js
@@ -0,0 +1,34 @@
+
+const navbar = document.getElementById('navbar');
+let prevScrollpos = window.scrollY;
+let scrolledDistance = 0; // To track how much scrolling was done since the navbar disappeared
+
+window.addEventListener('scroll', () => {
+    console.log(window.scrollY);
+    let currentScrollPos = window.scrollY;
+
+    if (currentScrollPos > prevScrollpos) {
+        // Scrolling down
+        scrolledDistance = 0;
+    } else {
+        // Scrolling up
+        scrolledDistance += (prevScrollpos - currentScrollPos);
+    }
+
+    if(currentScrollPos <= 80){
+        navbar.style.animation = 'fadeIn 0.5s';
+        navbar.classList.remove('transparent');
+    }else{
+        if (scrolledDistance > 100) {
+            navbar.style.animation = 'fadeIn 0.5s';
+            navbar.classList.remove('transparent');
+        } else {
+            navbar.style.animation = 'fadeOut 0.5s';
+            navbar.classList.add('transparent');
+        }
+    }
+
+
+
+    prevScrollpos = currentScrollPos;
+});
